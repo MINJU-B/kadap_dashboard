5151d49e23f2e0ba3a591d1a6fa5422f7e96daf6
MINJU <153149491+MINJU-B@users.noreply.github.com>
2025-08-20 11:07:03 +0900

    Update dashboard_user.py

--- dashboard_user.py
+++ dashboard_user.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from matplotlib import rc
 
-rc('font', familiy='MalgumGothic')
+rc('font', family='MalgumGothic')
 # plt.rcParams['font.family'] = 'NanumGothic'  # 한글 폰트 설정
 plt.rcParams['axes.unicode_minus'] = False   # 마이너스 기호 표시 설정
 
